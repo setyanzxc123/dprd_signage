739512de4043ae7eae01af74ce6f3b7814b33de2
Setya <setyanzxc@gmail.com>
2026-06-17 14:39:09 +0800

    Update initial data seeder defaults

--- app/Database/Seeds/InitialDataSeeder.php
+++ app/Database/Seeds/InitialDataSeeder.php
@@ -9,16 +9,20 @@ class InitialDataSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->deleteObsoleteSettings([
+            'logo_path',
+            'wa_notif_aktif',
+        ]);
+
         $this->insertMissing('settings', 'key_name', [
             ['key_name' => 'tema_signage',       'value' => 'dark'],
             ['key_name' => 'running_text',       'value' => 'Selamat datang di DPRD Provinsi Sulawesi Tengah'],
             ['key_name' => 'running_text_aktif', 'value' => '1'],
             ['key_name' => 'media_mode',         'value' => 'video'],
             ['key_name' => 'media_file',         'value' => ''],
-            ['key_name' => 'logo_path',          'value' => './assets/images/logo_dprd.jpg'],
-            ['key_name' => 'wa_notif_aktif',     'value' => '1'],
             ['key_name' => 'wa_sender_name',     'value' => 'Sekretariat DPRD'],
             ['key_name' => 'wa_template_reminder', 'value' => WhatsappService::defaultReminderTemplate()],
+            ['key_name' => 'wa_template_default_aktif', 'value' => '1'],
         ]);
 
         $this->insertMissing('users', 'username', [[
@@ -42,5 +46,16 @@ class InitialDataSeeder extends Seeder
                 $this->db->table($table)->insert($row);
             }
         }
+    }
+
+    private function deleteObsoleteSettings(array $keys): void
+    {
+        if (empty($keys)) {
+            return;
+        }
+
+        $this->db->table('settings')
+            ->whereIn('key_name', $keys)
+            ->delete();
     }
 }
